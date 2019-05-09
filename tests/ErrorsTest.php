61d782dc0480a6475c801e443f6cfd654921b7c3
jonaselan <jonas.elan@gmail.com>
2019-05-09 18:54:16 -0300

    Configure test suite

---
+++ tests/ErrorsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ErrorsTest extends TestCase
+{
+    /** @test */
+    public function assert_true()
+    {
+        $this->assertTrue(true);
+    }
+}
+?>
