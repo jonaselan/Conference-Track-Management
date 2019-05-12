af2bed282e1747ec571237e3ad40ef7173610d14
jonaselan <jonas.elan@gmail.com>
2019-05-11 22:11:53 -0300

    Set tests for validator

--- src/Helpers/messages.php
+++ src/Helpers/messages.php
@@ -2,9 +2,8 @@
 
 return [
     'en' => [
-        'input_non_exist' => 's',
-        'invalid_time_length' => 'i',
-        'title_has_integer' => 'm',
-        'default_error' => 'a',
+        'input_non_exist' => 'Invalid input: The file you are trying to open don\'t exist or can\'t be read',
+        'invalid_time_length' => 'Invalid input: A minimum amount of hours was not entered to fill a track',
+        'title_has_integer' => 'Invalid input: talk title can not have numbers',
     ]
 ];
